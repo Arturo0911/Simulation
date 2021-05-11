5143f4f8254d8e5dc29653917d7e23b7aa41e5db
arturo <anegreiross@outlook.com>
2021-05-10 20:33:23 -0500

    added gitignore

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
+	"github.com/Arturo0911/Simulation/evaluation"
 	"github.com/Arturo0911/Simulation/helpers"
 )
 
@@ -11,24 +13,23 @@ func CLI() {
 }
 
 func main() {
-
-	//coinDistribution1 := evaluation.NewDistribution(4, 3, 0.5)
-	//coinDistribution2 := evaluation.NewDistribution(4, 4, 0.5)
-	//fmt.Println(coinDistribution1.DistributionBinomial())
-	//fmt.Println(coinDistribution2.DistributionBinomial())
-	//
-	//c1, err := strconv.ParseFloat(coinDistribution1.DistributionBinomial(), 64)
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
-	//
-	//c2, err := strconv.ParseFloat(coinDistribution2.DistributionBinomial(), 64)
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
-	//
-	//fmt.Println(c1 + c2)
-	//
 	helpers.CLIBanner()
+
+	coinDistribution1 := evaluation.NewDistribution(4, 3, 0.5)
+	coinDistribution2 := evaluation.NewDistribution(4, 4, 0.5)
+	fmt.Println(coinDistribution1.DistributionBinomial())
+	fmt.Println(coinDistribution2.DistributionBinomial())
+
+	c1, err := strconv.ParseFloat(coinDistribution1.DistributionBinomial(), 64)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	c2, err := strconv.ParseFloat(coinDistribution2.DistributionBinomial(), 64)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(c1 + c2)
 
 }
