c70419a0b7a4973d09d7d667a19b7f45f46ee6d7
Arturo Negreiros <anegreiross@outlook.com>
2021-06-06 23:51:19 -0500

    everything ok

--- Proximation.py
+++ Proximation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 import random
+import math
 
 PRESENTATION = """
-        :::'###::::'########::'########::'########:::'#######::'##::::'##:'####:'##::::'##::::'###::::'########:'####::'#######::'##::: ##::'######::
-        ::'## ##::: ##.... ##: ##.... ##: ##.... ##:'##.... ##:. ##::'##::. ##:: ###::'###:::'## ##:::... ##..::. ##::'##.... ##: ###:: ##:'##... ##:
-        :'##:. ##:: ##:::: ##: ##:::: ##: ##:::: ##: ##:::: ##::. ##'##:::: ##:: ####'####::'##:. ##::::: ##::::: ##:: ##:::: ##: ####: ##: ##:::..::
-        '##:::. ##: ########:: ########:: ########:: ##:::: ##:::. ###::::: ##:: ## ### ##:'##:::. ##:::: ##::::: ##:: ##:::: ##: ## ## ##:. ######::
-         #########: ##.....::: ##.....::: ##.. ##::: ##:::: ##::: ## ##:::: ##:: ##. #: ##: #########:::: ##::::: ##:: ##:::: ##: ##. ####::..... ##:
-         ##.... ##: ##:::::::: ##:::::::: ##::. ##:: ##:::: ##:: ##:. ##::: ##:: ##:.:: ##: ##.... ##:::: ##::::: ##:: ##:::: ##: ##:. ###:'##::: ##:
-         ##:::: ##: ##:::::::: ##:::::::: ##:::. ##:. #######:: ##:::. ##:'####: ##:::: ##: ##:::: ##:::: ##::::'####:. #######:: ##::. ##:. ######::
-        ..:::::..::..:::::::::..:::::::::..:::::..:::.......:::..:::::..::....::..:::::..::..:::::..:::::..:::::....:::.......:::..::::..:::......:::
+    :::'###::::'########::'########::'########:::'#######::'##::::'##:'####:'##::::'##::::'###::::'########:'####::'#######::'##::: ##::'######::
+    ::'## ##::: ##.... ##: ##.... ##: ##.... ##:'##.... ##:. ##::'##::. ##:: ###::'###:::'## ##:::... ##..::. ##::'##.... ##: ###:: ##:'##... ##:
+    :'##:. ##:: ##:::: ##: ##:::: ##: ##:::: ##: ##:::: ##::. ##'##:::: ##:: ####'####::'##:. ##::::: ##::::: ##:: ##:::: ##: ####: ##: ##:::..::
+    '##:::. ##: ########:: ########:: ########:: ##:::: ##:::. ###::::: ##:: ## ### ##:'##:::. ##:::: ##::::: ##:: ##:::: ##: ## ## ##:. ######::
+     #########: ##.....::: ##.....::: ##.. ##::: ##:::: ##::: ## ##:::: ##:: ##. #: ##: #########:::: ##::::: ##:: ##:::: ##: ##. ####::..... ##:
+     ##.... ##: ##:::::::: ##:::::::: ##::. ##:: ##:::: ##:: ##:. ##::: ##:: ##:.:: ##: ##.... ##:::: ##::::: ##:: ##:::: ##: ##:. ###:'##::: ##:
+     ##:::: ##: ##:::::::: ##:::::::: ##:::. ##:. #######:: ##:::. ##:'####: ##:::: ##: ##:::: ##:::: ##::::'####:. #######:: ##::. ##:. ######::
+    ..:::::..::..:::::::::..:::::::::..:::::..:::.......:::..:::::..::....::..:::::..::..:::::..:::::..:::::....:::.......:::..::::..:::......:::
 """
 
 def presentation():
@@ -18,10 +19,19 @@ def presentation():
     print("[*] Owner Arturo Negreiros")
     print("[*] Follow me https://twitter.com/DevTuron\n")
 
-def aproximations(range_numbers:int, limit_a:int, limit_b:int)->float:
+def aproximations_int1(range_numbers:int, limit_a:int, limit_b:int)->float:
 
     random_float_numbers = [ (((limit_b - limit_a)*random.uniform(0,1))+limit_a)  for i in range(0,range_numbers)]
-    function_applied = [((value/2)+ value)  for value in random_float_numbers]
+    function_applied = [value/math.log(value) for value in random_float_numbers]
+    sum_float = sum(function_applied)/float(range_numbers)
+    
+    return sum_float
+
+
+def aproximations_int2(range_numbers:int, limit_a:int, limit_b:int)->float:
+
+    random_float_numbers = [ (((limit_b - limit_a)*random.uniform(0,1))+limit_a)  for i in range(0,range_numbers)]
+    function_applied = [  (((math.pow(value, 4))/4)+(5*math.pow(value, 3)/3)-(10*value))     for value in random_float_numbers]
     sum_float = sum(function_applied)/float(range_numbers)
     
     return sum_float
@@ -35,7 +45,15 @@ def CLI():
         range_a = int(input("input the limit a: "))
         range_b = int(input("input the limit b: "))
 
-        print(aproximations(range_numbers, range_a, range_b))
+        print("ln^x")
+        print(aproximations_int1(range_numbers, range_a, range_b),"\n")
+        
+
+        range_a = int(input("\ninput the limit a: "))
+        range_b = int(input("\ninput the limit b: "))
+
+        print("∫(X³ + 5x² - 10 dx)")
+        print(aproximations_int2(range_numbers, range_a, range_b))
     
     except:
         print("You should to input numbers no another format!")
